e4cc029029227fc3997ce19b54012f866a8bb97c
Walooh <Developer>
2020-01-14 00:04:00 -0300

    primeras pruebas
    Se cargan escenas y script con la UI

---
+++ Assets/Scripts/Movimiento.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movimiento : MonoBehaviour
+{
+    public float speed = 10.0F; //Velocidad de movimiento
+    public float rotationSpeed = 100.0F; //Velocidad de rotación
+
+    void Update()
+    {
+        transform.Translate(0, 0, Input.GetAxis("Vertical") * speed * Time.deltaTime);
+        transform.Rotate(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+    }
+    
+}
---
+++ Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    public Canvas _inventarioIn;
+    [SerializeField]
+    GameObject _inventarioActividad;
+    // Start is called before the first frame update
+
+    
+    private void Update()
+    {
+       activarInventario();
+    }
+
+    void activarInventario() {
+         bool prueba = _inventarioActividad;
+        if (Input.GetKey(KeyCode.I)){
+            
+            _inventarioActividad.GetComponent<Canvas> ().enabled = !_inventarioActividad.GetComponent<Canvas> ().enabled;
+            return;
+        }
+    }
+}
